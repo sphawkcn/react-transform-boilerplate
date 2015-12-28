d58cd81c784342b8a3754bdd48b214a6aeaf1eaa
Peter Wang <happypeter1983@gmail.com>
2015-12-28 20:33:31 +0800

    es6 module

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
-import './style/main.scss';
 
+class Point {
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')';
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
 }
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
-}
+export default Point;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import Point from './App';
 
-render(<App />, document.getElementById('root'));
+var pnt = new Point(2,3);
+
+console.log(pnt.toString());
